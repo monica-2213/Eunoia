58346c491c4cd0fb6efb53e7c82c7e6f755b715f
Qayleef <muhammadqayleef@hotmail.com>
2023-01-01 22:13:52 +0800

    UpdatedAsOfFirstJanuary2023

---
+++ app/src/main/java/com/example/eunoia/QuestionsAnswer.java
@@ -0,0 +1,22 @@
+package com.example.eunoia;
+
+public class QuestionsAnswer {
+
+    public static String question[]={
+            "Which company owns the android?",
+            "Which one is not the progamming language?",
+            "Where are you watching this video?"
+    };
+
+    public static String choice[][] ={
+            {"Google","Apple","Nokia"},
+            {"Java","Kotlin","Notepad"},
+            {"Facebook","Whatsapp","Youtube"}
+    };
+
+    public static String correctAnswers[] ={
+            "Google",
+            "Notepad",
+            "Youtube"
+    };
+}
