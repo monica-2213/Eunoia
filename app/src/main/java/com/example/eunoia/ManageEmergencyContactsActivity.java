db56a9194a3d0c0a96123ea8a941518eb585e6a7
Qayleef <muhammadqayleef@hotmail.com>
2023-01-06 01:37:02 +0800

    ForMonicaUpdatedAt1:36AM_On_6/1/2023

--- app/src/main/java/com/example/eunoia/ManageEmergencyContactsActivity.java
+++ app/src/main/java/com/example/eunoia/ManageEmergencyContactsActivity.java
@@ -7,41 +7,30 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ManageEmergencyContactsActivity#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class ManageEmergencyContactsActivity extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    EditText editText;
+    Button button;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+
 
     public ManageEmergencyContactsActivity() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ManageEmergencyContactsActivity.
-     */
+
     // TODO: Rename and change types and number of parameters
     public static ManageEmergencyContactsActivity newInstance(String param1, String param2) {
         ManageEmergencyContactsActivity fragment = new ManageEmergencyContactsActivity();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+
         fragment.setArguments(args);
         return fragment;
     }
@@ -49,16 +38,29 @@ public class ManageEmergencyContactsActivity extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_manage_emergency_contacts_activity, container, false);
+        View v = inflater.inflate(R.layout.fragment_manage_emergency_contacts_activity, container, false);
+        button = v.findViewById(R.id.BTNUpdate);
+        editText = v.findViewById(R.id.EmergencyContactName);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle bundle = new Bundle();
+                bundle.putString("key",editText.getText().toString());
+                EmergencyActivity fragment = new EmergencyActivity();
+                fragment.setArguments(bundle);
+                getFragmentManager().beginTransaction().replace(R.id.TVContact1,fragment).commit();
+
+            }
+        });
+        return v;
     }
 }
