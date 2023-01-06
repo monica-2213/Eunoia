d3a4f8e9e9d0e0db1d2567545764893da88db89f
tessashalini <106004937+tessashalini@users.noreply.github.com>
2023-01-07 01:58:42 +0800

    UpdatedOn070123

--- app/src/main/java/com/example/eunoia/recommendation.java
+++ app/src/main/java/com/example/eunoia/recommendation.java
@@ -2,63 +2,46 @@ package com.example.eunoia;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link recommendation#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class recommendation extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public recommendation() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment recommendation.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static recommendation newInstance(String param1, String param2) {
-        recommendation fragment = new recommendation();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+@Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState){
+    Button BtnMusic = view.findViewById(R.id.BtnMusic);
+    View.OnClickListener OCLMusic = new View.OnClickListener(){
+        @Override
+        public void onClick(View v){
+            Navigation.findNavController(view).navigate(R.id.action_recommendation_to_music);
         }
-    }
+    };
+    BtnMusic.setOnClickListener(OCLMusic);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recommendation, container, false);
-    }
+    Button BtnYoga = view.findViewById(R.id.BtnYoga);
+    View.OnClickListener OCLYoga = new View.OnClickListener(){
+        @Override
+        public void onClick(View v){
+            Navigation.findNavController(view).navigate(R.id.action_recommendation_to_yoga);
+        }
+    };
+    BtnYoga.setOnClickListener(OCLYoga);
+
+    Button BtnBreathing = view.findViewById(R.id.BtnBreathing);
+    View.OnClickListener OCLBreathing = new View.OnClickListener(){
+        @Override
+        public void onClick(View v){
+            Navigation.findNavController(view).navigate(R.id.action_recommendation_to_breathing_exercise2);
+        }
+    };
+    BtnYoga.setOnClickListener(OCLBreathing);
+
+}
+
 }
