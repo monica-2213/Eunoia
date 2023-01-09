4f3b4a34bd08425428ce47c43bc38dfb50f41909
Hazeeq 02 <hazeeqazahar@gmail.com>
2023-01-09 16:08:40 +0800

    Fixed some errors

--- app/src/main/java/com/example/eunoia/EmergencyActivity.java
+++ app/src/main/java/com/example/eunoia/EmergencyActivity.java
@@ -13,18 +13,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class EmergencyActivity extends AppCompatActivity {
 
-    TextView textview,numview,textview2,numview2,textview3,numview3;
+    TextView textview, numview, textview2, numview2, textview3, numview3;
     Button btnManageEm;
-    private static final String SHARED_PREF_NAME= "Eunoia";
-    private static final String CONTACT_NAME= "EmergencyName";
-    private static final String CONTACT_NUMBER= "EmergencyNumber";
-    private static final String CONTACT_NAME2= "EmergencyName2";
-    private static final String CONTACT_NUMBER2= "EmergencyNumber2";
-    private static final String CONTACT_NAME3= "EmergencyName3";
-    private static final String CONTACT_NUMBER3= "EmergencyNumber3";
+    private static final String SHARED_PREF_NAME = "Eunoia";
+    private static final String CONTACT_NAME = "EmergencyName";
+    private static final String CONTACT_NUMBER = "EmergencyNumber";
+    private static final String CONTACT_NAME2 = "EmergencyName2";
+    private static final String CONTACT_NUMBER2 = "EmergencyNumber2";
+    private static final String CONTACT_NAME3 = "EmergencyName3";
+    private static final String CONTACT_NUMBER3 = "EmergencyNumber3";
 
     @Override
-    public void onCreate(Bundle savedInstanceState){
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emergency);
 
@@ -36,15 +36,15 @@ public class EmergencyActivity extends AppCompatActivity {
         textview3 = findViewById(R.id.TVContact3);
         numview3 = findViewById(R.id.number3);
 
-        SharedPreferences sp = getApplicationContext().getSharedPreferences(SHARED_PREF_NAME,Context.MODE_PRIVATE);
+        SharedPreferences sp = getApplicationContext().getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
 
 
-        String Name1 = sp.getString(CONTACT_NAME,"");
-        String Num1 = sp.getString(CONTACT_NUMBER,"");
-        String Name2 = sp.getString(CONTACT_NAME2,"");
-        String Num2 = sp.getString(CONTACT_NUMBER2,"");
-        String Name3 = sp.getString(CONTACT_NAME3,"");
-        String Num3 = sp.getString(CONTACT_NUMBER3,"");
+        String Name1 = sp.getString(CONTACT_NAME, "");
+        String Num1 = sp.getString(CONTACT_NUMBER, "");
+        String Name2 = sp.getString(CONTACT_NAME2, "");
+        String Num2 = sp.getString(CONTACT_NUMBER2, "");
+        String Name3 = sp.getString(CONTACT_NAME3, "");
+        String Num3 = sp.getString(CONTACT_NUMBER3, "");
 
         textview.setText(Name1);
         numview.setText(Num1);
@@ -54,10 +54,10 @@ public class EmergencyActivity extends AppCompatActivity {
         numview3.setText(Num3);
 
         btnManageEm = findViewById(R.id.ButtonManageEmContacts);
-        btnManageEm .setOnClickListener(new View.OnClickListener(){
+        btnManageEm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(EmergencyActivity.this,ManageEmergencyContactsActivity.class);
+                Intent intent = new Intent(EmergencyActivity.this, ManageEmergencyContactsActivity.class);
                 startActivity(intent);
                 finish();
             }
@@ -65,3 +65,4 @@ public class EmergencyActivity extends AppCompatActivity {
 
 
     }
+}
