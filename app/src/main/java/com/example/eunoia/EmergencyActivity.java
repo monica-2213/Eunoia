1a379c1b2b972e0363729ba785c0c5c429172ea0
Qayleef <muhammadqayleef@hotmail.com>
2023-01-09 15:35:21 +0800

    Qayleef1/9/2023

--- app/src/main/java/com/example/eunoia/EmergencyActivity.java
+++ app/src/main/java/com/example/eunoia/EmergencyActivity.java
@@ -1,48 +1,67 @@
 package com.example.eunoia;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link EmergencyActivity#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class EmergencyActivity extends Fragment {
-    TextView textview;
-    public EmergencyActivity() {
-        // Required empty public constructor
-    }
+import androidx.appcompat.app.AppCompatActivity;
 
 
-    public static EmergencyActivity newInstance(String param1, String param2) {
-        EmergencyActivity fragment = new EmergencyActivity();
-        Bundle args = new Bundle();
+public class EmergencyActivity extends AppCompatActivity {
 
-        fragment.setArguments(args);
-        return fragment;
-    }
+    TextView textview,numview,textview2,numview2,textview3,numview3;
+    Button btnManageEm;
+    private static final String SHARED_PREF_NAME= "Eunoia";
+    private static final String CONTACT_NAME= "EmergencyName";
+    private static final String CONTACT_NUMBER= "EmergencyNumber";
+    private static final String CONTACT_NAME2= "EmergencyName2";
+    private static final String CONTACT_NUMBER2= "EmergencyNumber2";
+    private static final String CONTACT_NAME3= "EmergencyName3";
+    private static final String CONTACT_NUMBER3= "EmergencyNumber3";
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_emergency);
+
+
+        textview = findViewById(R.id.TVContact1);
+        numview = findViewById(R.id.number1);
+        textview2 = findViewById(R.id.TVContact2);
+        numview2 = findViewById(R.id.number2);
+        textview3 = findViewById(R.id.TVContact3);
+        numview3 = findViewById(R.id.number3);
+
+        SharedPreferences sp = getApplicationContext().getSharedPreferences(SHARED_PREF_NAME,Context.MODE_PRIVATE);
+
+
+        String Name1 = sp.getString(CONTACT_NAME,"");
+        String Num1 = sp.getString(CONTACT_NUMBER,"");
+        String Name2 = sp.getString(CONTACT_NAME2,"");
+        String Num2 = sp.getString(CONTACT_NUMBER2,"");
+        String Name3 = sp.getString(CONTACT_NAME3,"");
+        String Num3 = sp.getString(CONTACT_NUMBER3,"");
+
+        textview.setText(Name1);
+        numview.setText(Num1);
+        textview2.setText(Name2);
+        numview2.setText(Num2);
+        textview3.setText(Name3);
+        numview3.setText(Num3);
+
+        btnManageEm = findViewById(R.id.ButtonManageEmContacts);
+        btnManageEm .setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(EmergencyActivity.this,ManageEmergencyContactsActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
 
     }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_emergency_activity, container, false);
-        textview = v.findViewById(R.id.TVContact1);
-        Bundle bundle = this.getArguments();
-        String data = bundle.getString("key");
-        textview.setText(data);
-        return v;
-    }
-}
--- app/src/main/java/com/example/eunoia/ManageEmergencyContactsActivity.java
+++ app/src/main/java/com/example/eunoia/ManageEmergencyContactsActivity.java
@@ -1,66 +1,77 @@
 package com.example.eunoia;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
 
-public class ManageEmergencyContactsActivity extends Fragment {
+public class ManageEmergencyContactsActivity extends AppCompatActivity {
 
-    EditText editText;
-    Button button;
+    EditText editName1, editNum1, editName2, editNum2, editName3, editNum3;
+    Button Update,Back;
+    private static final String SHARED_PREF_NAME= "Eunoia";
+    private static final String CONTACT_NAME= "EmergencyName";
+    private static final String CONTACT_NUMBER= "EmergencyNumber";
+    private static final String CONTACT_NAME2= "EmergencyName2";
+    private static final String CONTACT_NUMBER2= "EmergencyNumber2";
+    private static final String CONTACT_NAME3= "EmergencyName3";
+    private static final String CONTACT_NUMBER3= "EmergencyNumber3";
 
-
-
-    public ManageEmergencyContactsActivity() {
-        // Required empty public constructor
-    }
-
-
-    // TODO: Rename and change types and number of parameters
-    public static ManageEmergencyContactsActivity newInstance(String param1, String param2) {
-        ManageEmergencyContactsActivity fragment = new ManageEmergencyContactsActivity();
-        Bundle args = new Bundle();
-
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-    }
-
+        setContentView(R.layout.activity_manage_emergency_contact);
 
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_manage_emergency_contacts_activity, container, false);
-        button = v.findViewById(R.id.BTNUpdate);
-        editText = v.findViewById(R.id.EmergencyContactName);
+        editName1 = findViewById(R.id.emergencyContactName);
+        editNum1 = findViewById(R.id.emergencyContactNumber);
+        editName2 = findViewById(R.id.emergencyContactName2);
+        editNum2 = findViewById(R.id.emergencyContactNumber2);
+        editName3 = findViewById(R.id.emergencyContactName3);
+        editNum3 = findViewById(R.id.emergencyContactNumber3);
+        Update = findViewById(R.id.BTNUpdate);
+        Back = findViewById(R.id.ToDisplay);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREF_NAME,Context.MODE_PRIVATE);
+
+        Update.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                Bundle bundle = new Bundle();
-                bundle.putString("key",editText.getText().toString());
-                EmergencyActivity fragment = new EmergencyActivity();
-                fragment.setArguments(bundle);
-                getFragmentManager().beginTransaction().replace(R.id.TVContact1,fragment).commit();
+                String EmName1 = editName1.getText().toString();
+                String EmNum1 = editNum1.getText().toString();
+                String EmName2 = editName2.getText().toString();
+                String EmNum2 = editNum2.getText().toString();
+                String EmName3 = editName3.getText().toString();
+                String EmNum3 = editNum3.getText().toString();
 
+
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString(CONTACT_NAME,editName1.getText().toString());
+                editor.putString(CONTACT_NUMBER,editNum1.getText().toString());
+                editor.putString(CONTACT_NAME2,editName2.getText().toString());
+                editor.putString(CONTACT_NUMBER2,editNum2.getText().toString());
+                editor.putString(CONTACT_NAME3,editName3.getText().toString());
+                editor.putString(CONTACT_NUMBER3,editNum3.getText().toString());
+                editor.commit();
+                Intent intent1 = new Intent(ManageEmergencyContactsActivity.this,EmergencyActivity.class);
+                startActivity(intent1);
             }
         });
-        return v;
+
+        Back.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ManageEmergencyContactsActivity.this,EmergencyActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
 }
