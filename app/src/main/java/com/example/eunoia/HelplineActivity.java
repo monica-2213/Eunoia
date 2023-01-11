3742a5e452d5cb3eabce09c7e1ab5249687d5a9b
Qayleef <muhammadqayleef@hotmail.com>
2023-01-11 18:43:22 +0800

    Qayleef1/11/2023

--- app/src/main/java/com/example/eunoia/HelplineActivity.java
+++ app/src/main/java/com/example/eunoia/HelplineActivity.java
@@ -1,5 +1,7 @@
 package com.example.eunoia;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +10,36 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.example.eunoia.databinding.ActivityHelpBinding;
+import com.example.eunoia.databinding.ActivityHelplineBinding;
 
 
 public class HelplineActivity extends AppCompatActivity{
+    ActivityHelplineBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_helpline);
+        binding = ActivityHelplineBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.BtnEmailLifeLine.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                String EmailLifeLine = binding.LifeLineEmail.getText().toString();
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.setData(Uri.parse("mailto:"));
+                intent.putExtra(Intent.EXTRA_EMAIL,EmailLifeLine);
+
+                if(intent.resolveActivity(getPackageManager()) != null){
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(HelplineActivity.this, "No App is Installed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
